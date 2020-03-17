f598c8e4d10c63b91b03eef4abf03658340c457e
Ben Chatelain <ben@octop.ad>
2020-03-17 10:53:47 -0600

    🎉 Replace hello world with initial view construction example

--- ThinkingInSwiftUI.playground/Contents.swift
+++ ThinkingInSwiftUI.playground/Contents.swift
@@ -2,8 +2,23 @@ import SwiftUI
 import PlaygroundSupport
 
 struct ContentView: View {
+    @State private var tapCount = 0
+
     var body: some View {
-        Text("Hello World")
+        VStack {
+            Button(action: { self.tapCount += 1 }, label: {
+                Text("Tap me!")
+                    .padding()
+                    .background(Color(.tertiarySystemFill))
+                    .cornerRadius(5)
+            }).padding()
+
+            if tapCount > 0 {
+                Text("You've tapped \(tapCount) times")
+            } else {
+                Text("You've not yet tapped")
+            }
+        }
     }
 }
 
